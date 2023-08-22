ab58f2f021dc047c231d2308dec1a3ab737eabf4
Vitaliy Galata <vitaliy.galata@tangramltd.com>
2023-08-22 14:25:17 +0300

    use timer

--- src/App.js
+++ src/App.js
@@ -1,44 +1,10 @@
-import { useState, useEffect } from 'react';
+import Timer from './components/Timer';
 import './App.css';
 
 function App() {
-  const [count, setСount] = useState(0);
-  let timerId;
-
-  const startTimer = () => {
-    timerId = setInterval(() => {
-      setСount((prevCount) => prevCount + 1);
-    }, 1000);
-  };
-
-  const resetTimer = () => {
-    clearInterval(timerId);
-    setСount(0);
-  };
-
-  const stopTimer = () => {
-    clearInterval(timerId);
-    localStorage.setItem('timer', count);
-  };
-
-  useEffect(() => {
-    localStorage.setItem('timer', count);
-
-    const userCount = localStorage.getItem('timer');
-    console.log(userCount);
-    if (userCount) {
-      setСount(+userCount);
-    }
-  }, [count]);
-
   return (
     <div className="App">
-      <button onClick={startTimer}>Start</button>
-      <button className="btn" onClick={stopTimer}>
-        Stop
-      </button>
-      {count}
-      <button onClick={resetTimer}>Reset</button>
+      <Timer />
     </div>
   );
 }
