7b7d0f9ac966658982cb0e00b787ccadc9f18597
Vitaliy Galata <vitaliy.galata@tangramltd.com>
2023-08-22 15:03:44 +0300

    use timer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Timer from './components/Timer';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [isVisible, setVisible] = useState(false);
   return (
     <div className="App">
-      <Timer />
+      <button onClick={() => setVisible(!isVisible)}>Toggle timer</button>
+      {isVisible && <Timer />}
     </div>
   );
 }
